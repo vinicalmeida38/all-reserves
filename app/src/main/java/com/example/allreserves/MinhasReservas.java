735aa73723d3cf63905b8e760dfdb6b80a90cf41
vinicalmeida <vinicalmeida38@gmail.com>
2021-11-15 14:48:40 -0300

    Lista de restaurantes e minhas reservas

---
+++ app/src/main/java/com/example/allreserves/MinhasReservas.java
@@ -0,0 +1,18 @@
+package com.example.allreserves;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MinhasReservas extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_minhas_reservas, container, false);
+    }
+}
