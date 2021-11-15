84c7ed94f71234989851d2ad0cdb6c83cb4ae59d
Fabio Eduardo Furquim <fabio.furquim851@gmail.com>
2021-11-15 15:58:03 -0300

    TelasFabio

--- app/src/main/java/com/example/allreserves/Login_.java
+++ app/src/main/java/com/example/allreserves/Login_.java
@@ -2,6 +2,7 @@ package com.example.allreserves;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Login_ extends AppCompatActivity {
--- app/src/main/java/com/example/allreserves/MainActivity.java
+++ app/src/main/java/com/example/allreserves/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.allreserves;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void proximaTela(View view){
+        Intent intent = new Intent(this, Login_.class);
+        startActivity(intent);
     }
 }
