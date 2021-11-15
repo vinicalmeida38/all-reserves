967a457f6c7bb6cf3e939b282ded448c2fb7635d
caiostato <zombieiron9@gmail.com>
2021-11-15 16:05:56 -0300

    arquivos java telas restaurante

---
+++ app/src/main/java/com/example/allreserves/TelaRestaurante.java
@@ -0,0 +1,14 @@
+package com.example.allreserves;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class TelaRestaurante extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_restaurante);
+    }
+}
