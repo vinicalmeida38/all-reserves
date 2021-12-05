db3ba9fdbba79a4d16022df1e6325d51c356fed1
furquim99 <59511753+furquim99@users.noreply.github.com>
2021-12-05 14:24:04 -0300

    Update LoginRestaurante.java

--- app/src/main/java/com/example/allreserves/telas/restaurante/login/LoginRestaurante.java
+++ app/src/main/java/com/example/allreserves/telas/restaurante/login/LoginRestaurante.java
@@ -1,16 +1,30 @@
-package com.example.allreserves.telas.restaurante.login;
+package com.example.allreserves.telas.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.allreserves.R;
+import com.example.allreserves.telas.cliente.login.LoginRestaurante;
+import com.example.allreserves.telas.restaurante.login.LoginRestaurante;
 
-public class LoginRestaurante extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_restaurante);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void acessarLoginRestaurante(View view){
+        Intent intent = new Intent(this, LoginRestaurante.class);
+        startActivity(intent);
+    }
+
+    public void acessarLoginCliente(View view){
+        Intent intent = new Intent(this, com.example.allreserves.telas.cliente.login.LoginRestaurante.class);
+        startActivity(intent);
     }
 }
